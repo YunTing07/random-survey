247c4d2d7ab8502eb11a617e25e84856767b6e01
YunTing07 <151818762+YunTing07@users.noreply.github.com>
2023-11-30 20:24:20 +0800

    Update url.js

--- url.js
+++ url.js
@@ -11,5 +11,6 @@ const urls = [
 	'https://forms.gle/K4WRam37yZyikKXSA',
 	'https://forms.gle/TWfsXc1YHU6HHLqk9',
 	'https://forms.gle/bmrGp6SpdEQW38mTA',
-	'https://forms.gle/ykAY9ubBgVKemnfM6'
+	'https://forms.gle/ykAY9ubBgVKemnfM6',
+	'https://forms.gle/MrAGpHKrsjoW5Ltc6'
 ];
